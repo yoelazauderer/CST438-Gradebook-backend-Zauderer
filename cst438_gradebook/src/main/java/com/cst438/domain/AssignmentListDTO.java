c02457345bfd19a0b8fb781fd272edee82ab7228
Yoela <yoelazauderer@gmail.com>
2021-09-19 20:41:34 -0700

    fixed add, edit and delete assignment methods

--- cst438_gradebook/src/main/java/com/cst438/domain/AssignmentListDTO.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/AssignmentListDTO.java
@@ -14,6 +14,8 @@ public class AssignmentListDTO {
 		public String courseTitle;
 		public int courseId;
 
+		public AssignmentDTO() {}
+		
 		public AssignmentDTO(int assignmentId, int courseId, String assignmentName, String dueDate,
 				String courseTitle) {
 			this.assignmentId = assignmentId;
